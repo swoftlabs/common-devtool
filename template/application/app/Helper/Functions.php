b42288a2eedc61ddc825d31fa4d294569ae357d0
inhere <in.798@qq.com>
2019-07-20 21:28:45 +0800

    up

---
+++ template/application/app/Helper/Functions.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+/**
+ * @return string
+ */
+function user_func(): string
+{
+    return 'hello';
+}
--- template/application/test/bootstrap.php
+++ template/application/test/bootstrap.php
@@ -1,3 +1,4 @@
 <?php declare(strict_types=1);
+
 // Composer autoload
 require_once dirname(__DIR__) . '/vendor/autoload.php';
